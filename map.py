8f275ba9db378f1d04ab8915a9d82635af8c773e
Oliver Schmidhauser <oli@glow.li>
2018-12-05 00:20:30 +0100

    fix width

--- map.py
+++ map.py
@@ -567,7 +567,8 @@ elif options.i:
     hshift=1.2
 
     height=round((1*scale*hshift)-1)
-    width=round(len(lines[-1])*scale*wshift)
+    line=lines[0].split("#")[0]
+    width=round(len(line)*scale*wshift)
     imbase=[
             "#!/usr/bin/env magick-script",
             "-size {}x{}".format(width,height),
