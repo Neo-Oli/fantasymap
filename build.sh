83fbd2beac89479d72aad3eab9f507089d88f20c
Oliver Schmidhauser <oli@glow.li>
2020-03-11 19:38:34 +0100

    build tiles

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ dependencies+=('make')
 dependencies+=('python3')
 dependencies+=('optipng')
 dependencies+=('ttf-dejavu')
-buildcommands[ANY]="make -j4 all;git add dist;git commit -m 'dist updated by builder';git push"
+buildcommands[ANY]="make -j4 tiles all;git add dist;git commit -m 'dist updated by builder';git push"
