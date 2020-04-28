8fdaee79408323c01521441bbfbf9698afc39928
Oliver Schmidhauser <oli@glow.li>
2020-04-28 22:35:40 +0200

    fix build command

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ dependencies+=('make')
 dependencies+=('python3')
 dependencies+=('optipng')
 dependencies+=('ttf-dejavu')
-buildcommands[master]="git branch -D builder||:;git checkout -b builder;make clean;make -j4 tiles all;git add --allt;git commit -m 'dist updated by builder';git push -f origin builder"
+buildcommands[master]="git branch -D builder||:;git checkout -b builder;make clean;make -j4 tiles all;git add --all;git commit -m 'dist updated by builder';git push -f origin builder"
