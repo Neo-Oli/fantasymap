f2ea6438050d12e1fb8b51fa8f25fd1fce9ac4f4
Oliver Schmidhauser <oli@glow.li>
2020-05-02 17:57:32 +0200

    build tiles last, so any errors show up before

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ dependencies+=('make')
 dependencies+=('python3')
 dependencies+=('optipng')
 dependencies+=('ttf-dejavu')
-buildcommands[master]="git branch -D builder||:;git checkout -b builder;make clean;make -j4 tiles all;git add --all;git commit -m 'dist updated by builder';git push -f origin builder"
+buildcommands[master]="git branch -D builder||:;git checkout -b builder;make clean;make -j4 all;make -j4 tiles;git add --all;git commit -m 'dist updated by builder';git push -f origin builder"
