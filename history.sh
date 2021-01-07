66b71e91268311f00f4afe72cc6c15cbd89bf8dd
glow <oli@glow.li>
2021-01-07 05:03:26 +0100

    history check if file contains error as sometimes there are unicode errors

--- history.sh
+++ history.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-commits=`git log --pretty=format:'%H' --all`
+commits=`git log --pretty=format:'%H' --all --reverse`
 old=""
 for c in $commits;do
     for name in map.map mapfile.map map;do
-        map="`git cat-file blob $c:$name 2>/dev/null`"
+        map="`git show $c:$name 2>/dev/null`"
         if [ "$?" == 0 ];then
             break
         fi
@@ -17,6 +17,10 @@ for c in $commits;do
             if [ ! -f "${filename}.map" ];then
                 mkdir -p history
                 echo "$map" > "${filename}.map"
+            fi
+            if ! python3 -c "open(\"${filename}.map\",\"r\").read()";then
+                echo "Error: File ${filename}.map not readable"
+                exit 1
             fi
         fi
     else
