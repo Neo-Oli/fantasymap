a1a9a1b5ddc639cad1307a512a3c4ce99b5f668b
glow <oli@glow.li>
2021-01-14 02:18:55 +0100

    add tests to build command

--- build.sh
+++ build.sh
@@ -5,4 +5,11 @@ dependencies+=('python3')
 dependencies+=('optipng')
 dependencies+=('ttf-dejavu')
 dependencies+=('ffmpeg')
-buildcommands[master]="export NOPROGRESS=true;git branch -D builder||:;git checkout -b builder;make clean;make -j4 all;make -j4 tiles;git add --all;git commit -m 'dist updated by builder';git push -f origin builder"
+buildcommands[master]="
+export NOPROGRESS=true;
+git branch -D builder||:;
+git checkout -b builder;
+make clean;
+make test all tiles -j4;
+git add --all;
+git commit -m 'dist updated by builder';git push -f origin builder"
