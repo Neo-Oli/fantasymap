a4338edcefccb0971bbe6db91e7d19f650701ecf
glow <oli@glow.li>
2021-04-12 18:53:11 +0200

    add make clean back to build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ buildcommands[master]="
 export NOPROGRESS=true;
 git branch -D builder||:;
 git checkout -b builder;
+make clean;
 make test all tiles -j4;
 git add --all;
 git commit -m 'dist updated by builder';git push -f origin builder"
